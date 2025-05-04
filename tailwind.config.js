6469e4bed52c3ae6d4d870ff3872499120a0d7f4
Randilachathuranga <randilachathuranga100@gmail.com>
2025-05-04 10:08:42 +0530

    add

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,html}"],
+  presets: [require("nativewind/preser")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
